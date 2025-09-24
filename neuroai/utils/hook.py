5eb626e5c483a38696652a363b745c450708ba98
mayukhdeb <mayukhmainak2000@gmail.com>
2025-09-24 12:18:02 -0400

    type assertion

--- neuroai/utils/hook.py
+++ neuroai/utils/hook.py
@@ -1,8 +1,10 @@
 from .getting_modules import get_layer_by_name
+import torch
 
 class ForwardHook:
     def __init__(self, model, hook_layer_name: str):
         layer = get_layer_by_name(model=model, layer_name=hook_layer_name)
+        assert isinstance(layer, torch.nn.Conv2d), f"Oops, you chose a layer that's not a Conv2d layer (it's: {type(layer)}). Try picking a Conv2d layer instead (feel free to call mayukh for help if you're stuck!)"
         self.hook = layer.register_forward_hook(self.hook_fn)
         self.input = None
         self.output = None
